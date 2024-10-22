6bff4a353cd6761478490f2fcd9213c9604f839b
Varsha-Swaminathan <varshaswaminathan98@gmail.com>
2024-10-22 16:30:42 -0500

    Removed some folders from tracking

--- chainlit_app/app.py
+++
@@ -1,7 +0,0 @@
-import chainlit as cl
-
-
-@cl.on_message
-def main(message: cl.Message):
-    result = message.content.title()  # Assuming 'content' is where the text is stored
-    cl.send_message(content=f"Sure, here is the message: {result}")
